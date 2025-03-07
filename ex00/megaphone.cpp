a0f3070d14b1fa53ed295ea3f8a92da09af74a5f
Harold Sorel <hsorel@shi-r5-p2.s19.be>
2025-03-07 16:06:40 +0100

    final final

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: haroldsorel <marvin@42.fr>                 +#+  +:+       +#+        */
+/*   By: hsorel <hsorel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 19:12:53 by haroldsorel       #+#    #+#             */
-/*   Updated: 2025/01/20 19:12:54 by haroldsorel      ###   ########.fr       */
+/*   Updated: 2025/03/07 16:04:09 by hsorel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ int main(int argc, char **argv)
         str = argv[i];
         for (size_t j = 0; j < str.length(); j++)
             std::putchar(std::toupper(str[j]));
-        if (i < argc - 1)
-            std::putchar(' ');
+        //if (i < argc - 1)
+        //    std::putchar(' ');
     }
     std::putchar('\n');
     return (0);
